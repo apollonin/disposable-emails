e00559399ad745d2d02d2e82fec69def3d34a439
vaidas-lungis <vaidas.lungis+github@gmail.com>
2016-11-11 00:22:58 +0200

    Add new property timeout for dig command

--- EmailChecker.php
+++ EmailChecker.php
@@ -4,6 +4,13 @@ namespace Hostinger;
 
 class EmailChecker
 {
+    private $timeout = 2;
+
+    public function setTimeout($value)
+    {
+        $this->timeout = $value;
+    }
+
     /**
      * Check if it's a valid email, ie. not a throwaway email.
      *
@@ -13,6 +20,10 @@ class EmailChecker
      */
     public function isValid($email)
     {
+        if (strlen($email) > 254 ){
+            return false;
+        }
+
         if (false === $email = filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return false;
         }
@@ -53,32 +64,46 @@ class EmailChecker
 
     public function getMxsRecords($domain)
     {
-        if ($this->exec_enabled()) {
-            exec("dig +noall +answer MX " . escapeshellarg($domain), $lines);
-            $output = [];
-            if (empty($lines)) {
-                return $output;
-            }
-            foreach ($lines as $line) {
-                $mxInfo   = explode(' ', preg_replace('!\s+!', ' ', $lines[0]));
-                $output[] = [
-                    'host'   => $mxInfo[0],
-                    'ttl'    => $mxInfo[1],
-                    'class'  => $mxInfo[2],
-                    'type'   => $mxInfo[3],
-                    'pri'    => $mxInfo[4],
-                    'target' => $mxInfo[5],
-                ];
-            }
-            return $output;
+        if ($this->execEnabled()) {
+            return $this->getMxRecordWithDigCmd($domain);
         }
         return dns_get_record($domain, DNS_MX);
 
     }
 
-    function exec_enabled()
+    public function execEnabled()
     {
         $disabled = explode(',', ini_get('disable_functions'));
         return !in_array('exec', $disabled);
+    }
+
+    /**
+     * Use shell command dig to get MX record of provided domain
+     * @param string $domain
+     * @return array - same return values as dns_get_record()
+     * @see dns_get_record()
+     */
+    public function getMxRecordWithDigCmd($domain)
+    {
+        $lines = [];
+        $command = 'dig +noall +answer +time=' . escapeshellarg($this->timeout) . ' MX ' . escapeshellarg($domain);
+        exec($command, $lines);
+        $output = [];
+        if (empty($lines)) {
+            return $output;
+        }
+
+        foreach ($lines as $line) {
+            $mxInfo   = explode(' ', preg_replace('!\s+!', ' ', $line));
+            $output[] = [
+                'host'   => $mxInfo[0],
+                'ttl'    => $mxInfo[1],
+                'class'  => $mxInfo[2],
+                'type'   => $mxInfo[3],
+                'pri'    => $mxInfo[4],
+                'target' => $mxInfo[5],
+            ];
+        }
+        return $output;
     }
 }
